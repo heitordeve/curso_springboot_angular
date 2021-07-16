49eca33773c2cb310228987eac20d6bd26a31232
@heitordeve <heitor.developer@outlook.com>
2021-07-16 15:15:50 -0300

    ► Development ◄
    1 - Instalando dependência Starter WEB
    2 - Criando classe MinhaConfiguration
    3 - Chamando classe MinhaConfiguration para exibir nome do projeto
    
    Signed-off-by: @heitordeve <heitor.developer@outlook.com>

---
+++ src/main/java/br/com/startupestudio/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package br.com.startupestudio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
