4990f0e4d7c2d2631bf4dfe0e3a5ea3ec4d6e883
@heitordeve <heitor.developer@outlook.com>
2021-07-18 11:24:23 -0300

    ► Minor Change ◄
    1 - Alterando nome da Coluna data cadastro
    
    Signed-off-by: @heitordeve <heitor.developer@outlook.com>

--- clientes/src/main/java/br/com/startupestudio/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/br/com/startupestudio/clientes/model/entity/Cliente.java
@@ -19,6 +19,6 @@ public class Cliente {
     @Column(nullable = false, length = 11)
     private String cpf;
 
-    @Column
+    @Column(name = "data_cadastro")
     private LocalDate dataCadastro;
 }
---
+++ clientes/src/main/java/br/com/startupestudio/clientes/model/entity/Servico.java
@@ -0,0 +1,25 @@
+package br.com.startupestudio.clientes.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+@Data
+public class Servico {
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @Column(nullable = false, length = 150)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @Column
+    private BigInteger valor;
+}
