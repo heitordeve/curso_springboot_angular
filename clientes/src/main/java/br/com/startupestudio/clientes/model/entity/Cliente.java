f153c580432e5c5658461359a5796b68585c4208
@heitordeve <heitor.developer@outlook.com>
2021-07-18 11:12:00 -0300

    ► Development ◄
    1 - Adicionando Lombok
    2 - Implementando na Classe Cliente
    
    Signed-off-by: @heitordeve <heitor.developer@outlook.com>

--- clientes/src/main/java/br/com/startupestudio/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/br/com/startupestudio/clientes/model/entity/Cliente.java
@@ -1,9 +1,12 @@
 package br.com.startupestudio.clientes.model.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Cliente {
 
     @Id
