581912e6dfc9ce3a4d2eb3a6307c91783b7b0943
@heitordeve <heitor.developer@outlook.com>
2021-07-18 18:45:08 -0300

    ► Development ◄
    1 - Atualizando arquivo de documentação das rotas
    2 - Retirando importações não utilizadas
    3 - Criando metódo para deletar Cliente
    
    Signed-off-by: @heitordeve <heitor.developer@outlook.com>

--- clientes/src/main/java/br/com/startupestudio/clientes/ClientesApplication.java
+++ clientes/src/main/java/br/com/startupestudio/clientes/ClientesApplication.java
@@ -1,12 +1,7 @@
 package br.com.startupestudio.clientes;
 
-import br.com.startupestudio.clientes.model.entity.Cliente;
-import br.com.startupestudio.clientes.model.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
